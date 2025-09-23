44ec3e6b2752bbf3e9811fd6015e7e060c7cb1e6
Alexander York <therealalexy@outlook.com>
2025-09-22 22:02:30 -0400

    Edited Database class and added json for classes

--- ChimerasCauldron/ChimerasCauldron/DatabaseInitializer.cs
+++ ChimerasCauldron/ChimerasCauldron/DatabaseInitializer.cs
@@ -21,22 +21,39 @@ namespace ChimerasCauldron
             SqliteCommand createRacesCommand = connection.CreateCommand();
             createRacesCommand.CommandText =
                 @"
-                    CREATE TABLE IF NOT EXISTS Races (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Name TEXT NOT NULL,
-                        Description TEXT,
-                        AbilityScoreIncrease TEXT,
-                        Traits TEXT
-                    );
-                    CREATE TABLE IF NOT EXISTS Classes (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Name TEXT NOT NULL,
-                        Description TEXT,
-                        HitDie INTEGER,
-                        PrimaryAbility TEXT,
-                        SavingThrows TEXT,
-                        Skills TEXT
-                    );
+                CREATE TABLE IF NOT EXISTS Classes (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL,
+                    Description TEXT NOT NULL,
+                    Hit_Dice INTEGER NOT NULL,
+                    Proficiency TEXT NOT NULL,
+                    Spellcasting_Ability TEXT,
+                    Prepared_Spells TEXT,
+                    Prepared_Spells_Change TEXT,
+                    Cantrip_Progression TEXT
+                );
+
+                INSERT INTO Classes (
+                    Id, 
+                    Name, 
+                    Description, 
+                    Hit_Dice,
+                    Proficiency, 
+                    Spellcasting_Ability,
+                    Prepared_Spells, 
+                    Prepared_Spells_Change, 
+                    Cantrip_Progression
+                ) VALUES (
+                    1,
+                    ""Artificer"",
+                    ""Artificer Description"",
+                    8,
+                    ""con,int"",
+                    ""int"",
+                    ""<level>/2+<intMod>"",
+                    ""restlong"",
+                    ""2,2,2,2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,4""
+                );                  
                 ";
             createRacesCommand.ExecuteNonQuery();
         }
---
+++ ChimerasCauldron/ChimerasCauldron/Utils/JSONKeyExtracter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChimerasCauldron.Utils
+{
+    internal static class JSONKeyExtracter
+    {
+        public static HashSet<string> GetAllKeys(string filePath)
+        {
+            using var stream = File.OpenRead(filePath);
+            using var document = JsonDocument.Parse(stream);
+            var keys = new HashSet<string>();
+            ExtractKeys(document.RootElement, keys);
+            return keys;
+        }
+
+        private static void ExtractKeys(JsonElement element, HashSet<string> keys)
+        {
+            switch (element.ValueKind)
+            {
+                case JsonValueKind.Object:
+                    foreach (var property in element.EnumerateObject())
+                    {
+                        keys.Add(property.Name);
+                        ExtractKeys(property.Value, keys);
+                    }
+                    break;
+                case JsonValueKind.Array:
+                    foreach (var item in element.EnumerateArray())
+                    {
+                        ExtractKeys(item, keys);
+                    }
+                    break;
+                    // Other types (string, number, etc.) do not have keys
+            }
+        }
+    }
+}
